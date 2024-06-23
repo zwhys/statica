ab4143ecd088450c974bd0c97da3891b5fa4b484
zwhys <tanziyan297@gmail.com>
2024-06-23 17:47:59 +0800

    Started work on the selection of workout button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react"
 import { Button, Box } from "@mui/material"
+import SelectExerciseType from "./selectExerciseType"
 
 function App() {
   return (
-    <Box textAlign="center" mt={2}>
-      <Button variant="contained" color="secondary">Not a button</Button>
-    </Box>
+    <SelectExerciseType/>
   )
 }
 
---
+++ src/selectExerciseType.tsx
@@ -0,0 +1,90 @@
+import {
+  Stack,
+  Button,
+  Box,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Typography,
+  Grid,
+  DialogTitle,
+  TextField
+} from "@mui/material"
+import React, { useState } from "react"
+
+export function SelectExerciseType() {
+  const [open, setOpen] = useState(false)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+  return (
+    <Box textAlign="center">
+      <Button variant="contained" color="primary" onClick={handleClickOpen}>
+        Create New Workout
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle sx={{}}>Create New Workout</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item>
+              <TextField
+                id="setnum"
+                label="No. of Sets"
+                variant="outlined"
+                placeholder="Eg. 3"
+                sx={{ width: 150 }}
+              />
+            </Grid>
+            <Grid item>
+              <Typography>X</Typography>
+            </Grid>
+            <Grid item>
+              <TextField
+                id="repnum"
+                label="No. of Reps"
+                variant="outlined"
+                placeholder="Eg. 10"
+                sx={{ width: 150 }}
+              />
+            </Grid>
+            <Grid item>
+              <Typography>of</Typography>
+            </Grid>
+            <Grid item>
+              <TextField
+                id="exertype"
+                label="Type of Exercise"
+                variant="outlined"
+                placeholder="Eg. Pushups"
+                sx={{ width: 150 }}
+              />
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Box sx={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+            <Button
+              variant="outlined"
+              color="primary"
+              fullWidth
+              sx={{ margin: 2 }}
+              onClick={handleClose}
+            >
+              Cancel
+            </Button>
+            <Button variant="contained" color="primary" fullWidth sx={{ margin: 2 }}>
+              Create
+            </Button>
+          </Box>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  )
+}
+
+export default SelectExerciseType
