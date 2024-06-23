a493c4cebbeacdb9f70ff4bdf1732f5fec9e2272
zwhys <tanziyan297@gmail.com>
2024-06-23 19:04:34 +0800

    Imporved noPage

--- src/pages/noPage.tsx
+++ src/pages/noPage.tsx
@@ -1,8 +1,25 @@
-import { Typography } from "@mui/material"
+import { Container, Typography, Link} from "@mui/material"
 import React from "react"
 
 export function NoPage() {
-  return <Typography>Wut U Doin Here!</Typography>
+  return (
+    <Container>
+          <Typography variant="h4" gutterBottom>
+            404 - Page Not Found
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Oops! The page you are looking for might have been removed, had its name changed,
+            or is temporarily unavailable.
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Please check the URL for any spelling mistakes, or{' '}
+            <Link href="/home">
+              return to the homepage
+            </Link>
+            .
+          </Typography>
+      </Container>
+  )
 }
 
 export default NoPage
