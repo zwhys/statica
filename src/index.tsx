e7fb6a3fd256ff699b1e8f126a3e8f0ce4b7bab3
zwhys <tanziyan297@gmail.com>
2024-06-23 20:41:31 +0800

    Enable the possibility for background image and colour

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { createTheme, ThemeProvider } from "@mui/material"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import { Box, createTheme, ThemeProvider } from "@mui/material"
 import { red, green, teal, deepPurple } from "@mui/material/colors"
-import App from './App';
-import ExerTractHeader from './components/header';
+import App from "./App"
+import ExerTractHeader from "./components/header"
+// import './App.css';
 
 const theme = createTheme({
   palette: {
@@ -22,15 +23,26 @@ const theme = createTheme({
   }
 })
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <ExerTractHeader/>
-        <App/>
+      <ExerTractHeader />
+      {/* <Box
+        sx={{
+          backgroundImage: `url(/background.jpg)`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          height: "100vh", // Adjust as needed
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "white" // Text color on top of the background
+        }}
+      >
+        <App /> 
+      </Box> */}
+      <App />
     </ThemeProvider>
   </React.StrictMode>
-);
-
+)
