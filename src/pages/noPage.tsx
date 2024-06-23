f472e64ce654313e8955a81254300a6175cf818c
tjingsheng <tanjs41200@gmail.com>
2024-06-23 18:26:14 +0800

    add exclamation mark

--- src/pages/noPage.tsx
+++ src/pages/noPage.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material"
 import React from "react"
 
 export function NoPage() {
-  return <Typography>Wut U Doin Here</Typography>
+  return <Typography>Wut U Doin Here!</Typography>
 }
 
 export default NoPage
