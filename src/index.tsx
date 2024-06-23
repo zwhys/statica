c753617ac0a30ddf4d2ec2b5f0c96f3321aa8e3f
zwhys <tanziyan297@gmail.com>
2024-06-23 13:10:18 +0800

    Created font and theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createTheme, ThemeProvider } from "@mui/material"
+import { red, green, teal } from "@mui/material/colors"
 import App from './App';
+
+const theme = createTheme({
+  palette: {
+    primary: teal,
+    error: red,
+    success: green
+  },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
