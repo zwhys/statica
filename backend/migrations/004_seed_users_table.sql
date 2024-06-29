e5fb82bbcce646f362b5989ea5ee48a9d13c953f
zwhys <tanziyan297@gmail.com>
2024-06-29 23:33:11 +0800

    same as previous commit

--- backend/migrations/004_seed_users_table.sql
+++ backend/migrations/004_seed_users_table.sql
@@ -1,4 +1,4 @@
 INSERT INTO
     users (username, hashed_password)
 VALUES
-    ('mochi', '$2b$10$Ks6X4.AEt3nISBqmYILqiuPm5TsBnspH8ylQXc2BFt0PD4f40Pz/.'),
+    ('mochi', '$2b$10$Ks6X4.AEt3nISBqmYILqiuPm5TsBnspH8ylQXc2BFt0PD4f40Pz/.')
--- backend/migrations/006_seed_records_table.sql
+++ backend/migrations/006_seed_records_table.sql
@@ -1,11 +1,11 @@
 INSERT INTO records (user_id, date_of_entry, exercise_type, sets, reps, remarks) VALUES
     (1, '2024-06-26', 'Push-Up', 3, 15, 'Feeling good today'),
-    (2, '2024-06-25', 'Sit-Up', 2, 20, 'Worked on form'),
+    (1, '2024-06-25', 'Sit-Up', 4, 20, 'Worked on form'),
     (1, '2024-06-24', 'Bicep Curl', 4, 12, 'Increased weight'),
-    (2, '2024-06-26', 'Pull-Up', 3, 8, 'Improved grip strength'),
+    (1, '2024-06-26', 'Pull-Up', 3, 8, 'Improved grip strength'),
     (1, '2024-06-25', 'Deadlift', 5, 5, 'New personal record'),
-    (3, '2024-06-24', 'Bench Press', 4, 10, 'Added extra set'),
+    (1, '2024-06-24', 'Bench Press', 4, 10, 'Added extra set'),
     (1, '2024-06-23', 'Dips', 3, 12, 'Superset with pushups'),
-    (2, '2024-06-22', 'Shoulder Press', 3, 12, 'Focused on form'),
-    (3, '2024-06-21', 'Leg Press', 4, 15, 'Increased weight'),
+    (1, '2024-06-22', 'Shoulder Press', 3, 12, 'Focused on form'),
+    (1, '2024-06-21', 'Leg Press', 4, 15, 'Increased weight'),
     (1, '2024-06-20', 'Plank', 1, 60, 'Core workout');
