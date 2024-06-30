3def7dd7144ea0a52a51b15daaa4ea40318e7b7f
zwhys <tanziyan297@gmail.com>
2024-06-30 21:53:09 +0800

    Beautify it

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,45 +1,55 @@
-//TODO: Fix logging in feature
-
 import React, { useState } from "react"
 import { useForm, SubmitHandler } from "react-hook-form"
-import {
-  Button,
-  Dialog,
-  DialogContent,
-  Typography,
-  Link,
-  TextField,
-} from "@mui/material"
+import { Button, Dialog, DialogContent, Typography, TextField, Grid, Box } from "@mui/material"
 
-export const LogIn: React.FC<Props> = ({ open, onClose }) => {
+const LogIn: React.FC<Props> = ({ open, onClose }) => {
   const [isDialogOpen, setIsDialogOpen] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
   const {
-    control,
     register,
     handleSubmit,
     formState: { errors },
     reset,
   } = useForm<UserFormValues>()
-  const onSubmit: SubmitHandler<UserFormValues> = async data => {
+
+  const checkIsAuthenticated = async (username: string, password: string) => {
     try {
-      const response = await fetch("http://localhost:3001/login", {
+      const response = await fetch("http://localhost:3001/authentication", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          data,
-        }),
+        body: JSON.stringify({ username, password }),
       })
 
       if (!response.ok) {
-        throw new Error("Invalid password")
+        throw new Error("Authentication failed")
       }
+
+      const result = await response.json()
+      return result.isLogInDetailsValid
+    } catch (error) {
+      console.error("Error checking authentication:", error)
+      return false
+    }
+  }
+
+  const onSubmit: SubmitHandler<UserFormValues> = async data => {
+    try {
+      const isAuthenticated = await checkIsAuthenticated(data.username, data.password)
+
+      if (!isAuthenticated) {
+        // Handle authentication failure, show error message, etc.
+        return
+      }
+
+      setIsAuthenticated(true)
       onClose()
       reset()
-      window.location.href = "/calendar";
+      window.location.href = "/calendar"
     } catch (error) {
-      console.error("Error logging in:", error)
+      console.error("Error during login:", error)
     }
   }
 
@@ -55,42 +65,49 @@ export const LogIn: React.FC<Props> = ({ open, onClose }) => {
       </Button>
       <Dialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <DialogContent>
+          <DialogContent sx={{ height: 600, width: 900 }}>
             <Typography variant="h5" component="h1" gutterBottom>
               Log in to your account
             </Typography>
-            <TextField
-              label="Username"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              {...register("username", {
-                required: "Required", maxLength: {
-                  value: 50, 
-                  message: 'Username cannot exceed 50 characters'
-                }
-              })}
-              error={!!errors.username}
-              helperText={errors.username ? errors.username.message : ""}
-            />
-           <TextField
-              label="Password"
-              variant="outlined"
-              type="password"
-              fullWidth
-              margin="normal"
-              {...register("password", {
-                required: "Required",
-                },
-              )}
-              error={!!errors.password}
-              helperText={errors.password ? errors.password.message : ""}
-            />
-            <Link href="/calendar">
-              <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
+            <Box>
+              <TextField
+                label="Username"
+                variant="outlined"
+                margin="normal"
+                {...register("username", {
+                  required: "Required",
+                  maxLength: {
+                    value: 50,
+                    message: "Username cannot exceed 50 characters",
+                  },
+                })}
+                error={!!errors.username}
+                helperText={errors.username ? errors.username.message : ""}
+              />
+              <TextField
+                label="Password"
+                variant="outlined"
+                type="password"
+                margin="normal"
+                {...register("password", {
+                  required: "Required",
+                  validate: async value => {
+                    if (!isAuthenticated) {
+                      return "Incorrect Password."
+                    }
+                    return true
+                  },
+                })}
+                error={!!errors.password}
+                helperText={errors.password ? errors.password.message : ""}
+              />
+              <Button type="submit" variant="contained" color="primary" sx={{ mt: 2 }}>
                 Login
               </Button>
-            </Link>
+            </Box>
+            <Box>
+              
+            </Box>
           </DialogContent>
         </form>
       </Dialog>
@@ -99,3 +116,4 @@ export const LogIn: React.FC<Props> = ({ open, onClose }) => {
 }
 
 export default LogIn
+//TODO: Fix logging in feature
--- frontend/src/components/signup.tsx
+++ frontend/src/components/signup.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
 import { useForm, SubmitHandler } from "react-hook-form"
-import { Button, Dialog, DialogContent, Typography, TextField } from "@mui/material"
-
+import { Button, Dialog, Box, Typography, TextField } from "@mui/material"
 
 const SignUp: React.FC<Props> = ({ open, onClose }) => {
   const [isDialogOpen, setIsDialogOpen] = useState(false)
@@ -65,54 +64,76 @@ const SignUp: React.FC<Props> = ({ open, onClose }) => {
       >
         Sign Up
       </Button>
-      <Dialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
+      <Dialog
+        open={isDialogOpen}
+        onClose={() => setIsDialogOpen(false)}
+        maxWidth="md"
+        sx={{
+          "& .MuiDialog-paper": {
+            borderRadius: 8,
+          },
+        }}
+      >
         <form onSubmit={handleSubmit(onSubmit)}>
-          <DialogContent>
-            <Typography variant="h5" component="h1" gutterBottom>
-              Create an account
-            </Typography>
-            <TextField
-              label="Username"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              {...register("username", {
-                required: "Required",
-                maxLength: {
-                  value: 50,
-                  message: "Username cannot exceed 50 characters",
-                },
-                validate: async value => {
-                  const isUnique = await checkUniqueUsername(value)
-                  if (!isUnique) {
-                    return "Username is taken. Please choose another"
-                  }
-                  return true
-                },
-              })}
-              error={!!errors.username}
-              helperText={errors.username ? errors.username.message : ""}
+          <Box display="flex" height={500} width={722} bgcolor="mediumpurple">
+            <Box
+              flex={1}
+              display="flex"
+              justifyContent="center"
+              flexDirection="column"
+              sx={{ margin: 3 }}
+            >
+              <Typography variant="h4" sx={{ textAlign: "left", marginBottom: 2 }}>
+                Create an account
+              </Typography>
+              <TextField
+                label="Username"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                {...register("username", {
+                  required: "Required",
+                  maxLength: {
+                    value: 50,
+                    message: "Username cannot exceed 50 characters",
+                  },
+                  validate: async value => {
+                    const isUnique = await checkUniqueUsername(value)
+                    if (!isUnique) {
+                      return "Username is taken. Please choose another"
+                    }
+                    return true
+                  },
+                })}
+                error={!!errors.username}
+                helperText={errors.username ? errors.username.message : ""}
+              />
+              <TextField
+                label="Password"
+                variant="outlined"
+                type="password"
+                fullWidth
+                margin="normal"
+                {...register("password", {
+                  required: "Required",
+                  minLength: {
+                    value: 8,
+                    message: "Password must be at least 8 characters long",
+                  },
+                })}
+                error={!!errors.password}
+                helperText={errors.password ? errors.password.message : ""}
+              />
+              <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
+                Sign Up
+              </Button>
+            </Box>
+            <img
+              src="/login.png"
+              alt="Login Image"
+              style={{ maxWidth: "100%", maxHeight: "100%" }}
             />
-            <TextField
-              label="Password"
-              variant="outlined"
-              type="password"
-              fullWidth
-              margin="normal"
-              {...register("password", {
-                required: "Required",
-                minLength: {
-                  value: 8,
-                  message: "Password must be at least 8 characters long",
-                },
-              })}
-              error={!!errors.password}
-              helperText={errors.password ? errors.password.message : ""}
-            />
-            <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
-              Sign Up
-            </Button>
-          </DialogContent>
+          </Box>
         </form>
       </Dialog>
     </>
