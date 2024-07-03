431e1acf052ca785c99121ec1d47cbbb0f437411
zwhys <tanziyan297@gmail.com>
2024-07-03 15:47:53 +0800

    added colour

--- backend/migrations/002_create_exercise_types_table.sql
+++ backend/migrations/002_create_exercise_types_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE
     exercise_types (
         id SERIAL PRIMARY KEY NOT NULL,
-        exercise_type VARCHAR(50) UNIQUE NOT NULL
+        exercise_type VARCHAR(50) UNIQUE NOT NULL,
+        colour VARCHAR(7) NOT NULL
     );
