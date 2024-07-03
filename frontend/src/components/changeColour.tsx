72532dc9732c4634c96a2af8e868270078c27785
zwhys <tanziyan297@gmail.com>
2024-07-03 21:37:14 +0800

    done with ui for color picker

--- frontend/src/components/changeColour.tsx
+++ frontend/src/components/changeColour.tsx
@@ -2,6 +2,7 @@ import React, { useState, useCallback } from "react"
 import { ChromePicker, ColorResult } from "react-color"
 import { Box, Button, Dialog, Grid, IconButton, Typography } from "@mui/material"
 import SettingsIcon from "@mui/icons-material/Settings"
+import { Fullscreen } from "@mui/icons-material"
 
 export const ColorPicker: React.FC<Props> = ({ open, onClose }) => {
   const [color, setColor] = useState<string>("#000000")
@@ -29,7 +30,29 @@ export const ColorPicker: React.FC<Props> = ({ open, onClose }) => {
           <Typography variant="h5" sx={{ textAlign: "left", marginBottom: 2 }}>
             Update Colour
           </Typography>
-          <ChromePicker color={color} onChange={handleChange} disableAlpha={true} />
+
+          <Grid container spacing={2}>
+            <Grid item>
+              <ChromePicker color={color} onChange={handleChange} disableAlpha={true} />
+            </Grid>
+            <Grid item>
+              <Box
+                sx={{
+                  margin: 2,
+                  padding: 1,
+                  bgcolor: color,
+                  borderRadius: 2,
+                  height: 150,
+                  width: 150, // Fixed width
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography>Preview: {color}</Typography>
+              </Box>
+            </Grid>
+          </Grid>
           <Grid item xs={12}>
             <Grid container justifyContent="flex-end" spacing={2}>
               <Grid item>
@@ -41,7 +64,6 @@ export const ColorPicker: React.FC<Props> = ({ open, onClose }) => {
                 <Button
                   type="submit"
                   variant="contained"
-                  color="primary"
                   sx={{
                     backgroundImage: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
                     color: "white",
