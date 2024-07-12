ae5f83146b87b0892e1c9bd3be8917ae25c84b77
zwhys <tanziyan297@gmail.com>
2024-07-12 21:03:38 +0800

    working on displaying username

--- frontend/src/components/layout/viewCalendar.tsx
+++ frontend/src/components/layout/viewCalendar.tsx
@@ -1,20 +1,47 @@
-import React, { useState } from "react"
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react"
+import { useSelector } from "react-redux"
 import { Box, Typography, Button } from "@mui/material"
 import AddExerciseEntry from "../addExerciseEntry"
 import Calendar from "../calendar"
-
+import { RootState } from "../../redux/store"
 
 export function ViewCalendar() {
   const [isDialogOpen, setIsDialogOpen] = useState(false)
-  const userId = useSelector((state: any) => state.user.userId);
+  const [username, setUsername] = useState<string | null>(null)
+  const userId = useSelector((state: RootState) => state.user.userId)
+
+  useEffect(() => {
+    const fetchUsername = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/username", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userId }),
+        })
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch username")
+        }
+
+        const data = await response.json()
+        setUsername(data.username) // Update state with fetched username
+      } catch (error) {
+        console.error("Error fetching username:", error)
+      }
+    }
+
+    fetchUsername()
+  }, [userId])
+
   return (
     <>
       <AddExerciseEntry open={isDialogOpen} onClose={() => setIsDialogOpen(false)} />
 
       <Box sx={{ width: "25%" }}>
         <Typography variant="h3" sx={{ margin: 3 }}>
-          Welcome, {userId}
+          Welcome, {username !== null ? username : "Loading..."}
         </Typography>
         <Button
           sx={{
