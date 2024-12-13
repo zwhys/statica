f014fa47497c365cbc3c7bb3f1d84040ddf2044d
zwhys <tanziyan297@gmail.com>
2024-12-13 20:00:42 +0800

    added header to notFoundPage

--- frontend/src/pages/notFoundPage.tsx
+++ frontend/src/pages/notFoundPage.tsx
@@ -1,11 +1,23 @@
 import { Box, Typography, useTheme, Button } from "@mui/material"
+import { useSelector } from "react-redux"
+import HeaderLanding from "../components/layout/headerLanding"
+import HeaderHome from "../components/layout/headerHome"
 import Footer from "../components/layout/footer"
+import { RootState } from "../redux/store"
+import { useState } from "react"
 
 export function NotFoundPage() {
+  const userId = useSelector((state: RootState) => state.user.userId)
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
   const theme = useTheme()
 
   return (
     <>
+      {userId ? (
+        <HeaderHome setIsDrawerOpen={setIsDrawerOpen} isDrawerOpen={isDrawerOpen} />
+      ) : (
+        <HeaderLanding />
+      )}
       <Box textAlign="center">
         <img src="/404.png" alt="error" style={{ height: "100vh" }} />
         <Typography
